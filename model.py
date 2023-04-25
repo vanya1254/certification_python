4f44daff2a2260d81fe09de020761fd2ce2f5117
IVAN <vanya1254@yandex.ru>
2023-04-25 12:15:27 +0300

    add init into model

--- model.py
+++ model.py
@@ -5,4 +5,22 @@ class model():
     """отвечает за любое взаимодействие с данными"""
 
     def __init__(self):
-        self.db = json.load("database")
+        try:
+            with open("database.json", "r", encoding="UTF8") as db:
+                self.data = json.load(db)
+            print("File open.")
+        except FileNotFoundError:
+            print("File 'database.json' not found.\nCreated new.")
+            with open("database.json", "w", encoding="UTF8") as db:
+                sample_data = {"count": 0, "notes": [{}]}
+                json.dump(sample_data, db)
+                self.data = sample_data
+
+
+    def get_data(self):
+        return self.data
+
+
+model = model()
+
+print(model.get_data())
